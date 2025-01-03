34d68e1f3827b564eb95211cdc09023193cc3649
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 12:44:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -40,40 +40,40 @@
 
                     <?php $count++;
                 } ?>
+
+
+
+
+
+                <!-- fim post -->
+
+
+
+                <!-- Pagination & action buttons -->
+                <div class="container link-area">
+                    <div class="row links-section">
+                        <div class="col-sm-4 col-xs-12 col-sm-push-4 clearfix text-center">
+                            <ul class="list-inline links-holder">
+                                <li>
+                                    <a href="#" data-toggle="modal" data-target="#myModal">Share</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="pag pag-prev col-sm-4 col-xs-6 col-sm-pull-4">
+                            <a href="<?php previous_posts_link('Older posts'); ?>" class="link prev-post">
+                                <span class="arrow left-arrow"></span>Older Posts
+                            </a>
+                        </div>
+                        <div class="pag pag-next col-sm-4 col-xs-6 text-right">
+                            <a href="<?php next_posts_link('Newer posts'); ?>" class="link add next-post">Newer Posts
+                                <span class="arrow right-arrow"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./Pagination & action buttons -->
             <?php endwhile; ?>
         <?php endif; ?>
-
-
-
-
-        <!-- fim post -->
-
-
-
-        <!-- Pagination & action buttons -->
-        <div class="container link-area">
-            <div class="row links-section">
-                <div class="col-sm-4 col-xs-12 col-sm-push-4 clearfix text-center">
-                    <ul class="list-inline links-holder">
-                        <li>
-                            <a href="#" data-toggle="modal" data-target="#myModal">Share</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="pag pag-prev col-sm-4 col-xs-6 col-sm-pull-4">
-                    <a href="<?php previous_posts_link('Older posts'); ?>" class="link prev-post">
-                        <span class="arrow left-arrow"></span>Older Posts
-                    </a>
-                </div>
-                <div class="pag pag-next col-sm-4 col-xs-6 text-right">
-                    <a href="<?php next_posts_link('Newer posts'); ?>" class="link add next-post">Newer Posts
-                        <span class="arrow right-arrow"></span>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <!-- ./Pagination & action buttons -->
-
         <!-- Go top button/arrow -->
         <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
 
