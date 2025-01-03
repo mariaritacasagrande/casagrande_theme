df2affc6fc1f64197b90175d45ba52005532b0d0
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 15:39:44 -0300

    update php

--- php/form-process.php
+++ php/form-process.php
@@ -7,14 +7,14 @@ $message = $_POST["message"];
 // YOU ONLY NEED TO MODIFY THIS TWO VARIABLES
 // WITH YOUR MAIL ACCOUNT AND CUSTOMIZE YOUR
 // SUBJECT ONCE THE MAIL ARRIVES TO YOUR INBOX
-$EmailTo = "yourmailhere@gmail.com";
+$EmailTo = "olamariarita@gmail.com";
 $Subject = "New Message Received from your Website!";
- 
+
 // prepare email body text
 $Body .= "Name: ";
 $Body .= $name;
 $Body .= "\n";
- 
+
 $Body .= "Email: ";
 $Body .= $email;
 $Body .= "\n";
@@ -22,19 +22,19 @@ $Body .= "\n";
 $Body .= "Topic: ";
 $Body .= $topic;
 $Body .= "\n";
- 
+
 $Body .= "Message: ";
 $Body .= $message;
 $Body .= "\n";
- 
+
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
- 
+$success = mail($EmailTo, $Subject, $Body, "From:" . $email);
+
 // redirect to success page
-if ($success){
-   echo "success";
-}else{
+if ($success) {
+    echo "success";
+} else {
     echo "invalid";
 }
- 
+
 ?>
--- php/newsletter-process.php
+++ php/newsletter-process.php
@@ -4,22 +4,22 @@ $email = $_POST["email"];
 // YOU ONLY NEED TO MODIFY THIS TWO VARIABLES
 // WITH YOUR MAIL ACCOUNT AND CUSTOMIZE YOUR
 // SUBJECT ONCE THE MAIL ARRIVES TO YOUR INBOX
-$EmailTo = "yourmailhere@gmail.com";
+$EmailTo = "olamariarita@gmail.com";
 $Subject = "New Subscriber from your Website!";
- 
+
 // prepare email body text
 $Body .= "Subscriber mail: ";
 $Body .= $email;
 $Body .= "\n";
- 
+
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
- 
+$success = mail($EmailTo, $Subject, $Body, "From:" . $email);
+
 // redirect to success page
-if ($success){
-   echo "success";
-}else{
+if ($success) {
+    echo "success";
+} else {
     echo "invalid";
 }
- 
+
 ?>
