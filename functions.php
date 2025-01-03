269a7ae0b58ff120807b84ea222609c45dd38d33
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 14:26:46 -0300

    Updates

--- functions.php
+++ functions.php
@@ -95,7 +95,44 @@ add_theme_support('html5', array('style', 'script'));
 add_theme_support('automatic-feed-links');
 
 //prev-next
+function getPrevNext()
+{
+    $pagelist = get_pages('sort_column=menu_order&sort_order=asc');
+    $pages = array();
+    foreach ($pagelist as $page) {
+        $pages[] += $page->ID;
+    }
 
+    $current = array_search(get_the_ID(), $pages);
+    $prevID = $pages[$current - 1];
+    $nextID = $pages[$current + 1];
+
+    echo '<div class="navigation">';
+
+    if (!empty($prevID)) {
+        echo '<div class="pag pag-prev col-sm-4 col-xs-6 col-sm-pull-4">';
+        echo '<a href="';
+        echo get_permalink($prevID);
+        echo '"';
+        echo 'title="';
+        echo get_the_title($prevID);
+        echo 'class="link prev-post"><span class="arrow left-arrow"></span>Older Posts
+                        </a>';
+        echo "</div>";
+    }
+    if (!empty($nextID)) {
+        echo '<div class="pag pag-next col-sm-4 col-xs-6 text-right">';
+        echo '<a href="';
+        echo get_permalink($nextID);
+        echo '"';
+        echo 'title="';
+        echo get_the_title($nextID);
+        echo 'class="link add next-post">Newer Posts
+                            <span class="arrow right-arrow"></span>
+                        </a>';
+        echo "</div>";
+    }
+}
 
 
 ?>
