7138b8e2cb75c1f3559f5397733c5acbb8393ae8
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 20:30:57 -0300

    Update

---
+++ project.php
@@ -0,0 +1,118 @@
+<?php
+/*
+Template Name: Project
+*/
+?>
+
+<!-- Main content -->
+<main id="main">
+    <!-- Page head/intro title & subtitle -->
+    <div class="container heading-section">
+        <div class="intro big-heading parallax col-sm-12">
+            <h1>People by Leif</h1>
+            <p class="feat-text">People are people is the last project that we have developed for Schomedde, a very
+                talented german photographer and a great partner.</p>
+            <ul class="list-inline meta-cats">
+                <li><a href="#">Branding</a></li>
+                <li><a href="#">Identity</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="projects-section">
+        <!-- Project contents -->
+        <div class="container">
+            <div class="media-container">
+                <img src="images/projects/project-01/img21.jpg" width="1380" height="813" alt="image description"
+                    class="img-responsive lazy">
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 media-container">
+                    <div class="video-inner">
+                        <div class="video-wrapper">
+                            <div>
+                                <iframe src="//www.youtube.com/embed/coyi-iubCJo" width="560" height="315"
+                                    allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 media-container">
+                    <div class="video-inner">
+                        <div class="video-wrapper">
+                            <div>
+                                <iframe src="//player.vimeo.com/video/117760906" width="560" height="315"
+                                    allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 media-container">
+                    <img src="images/projects/project-01/img23.jpg" height="550" width="675" alt="image description"
+                        class="img-responsive lazy">
+                </div>
+                <div class="col-sm-6 media-container">
+                    <img src="images/projects/project-01/img24.jpg" height="550" width="675" alt="image description"
+                        class="img-responsive lazy">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 media-container">
+                    <img src="images/projects/project-01/img25.jpg" width="675" height="550" alt="image description"
+                        class="img-responsive lazy">
+                    <div class="caption">
+                        <div class="holder">Another cool caption</div>
+                    </div>
+                </div>
+                <div class="col-sm-6 media-container">
+                    <img src="images/projects/project-01/img26.jpg" width="675" height="550" alt="image description"
+                        class="img-responsive lazy">
+                </div>
+            </div>
+            <div class="row">
+                <div class="centered-content-vh col-sm-6 col-sm-offset-3">
+                    <h3 class="highlight">The Magazine</h3>
+                    <p>The process continues with all the identity implementation<br>in different supports</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 media-container">
+                    <img src="images/projects/project-01/img27.jpg" width="1380" height="1035" alt="image description"
+                        class="img-responsive lazy">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ./Project contents -->
+
+    <!-- Pagination & action buttons -->
+    <div class="container link-area">
+        <div class="row links-section">
+            <div class="col-sm-4 col-xs-12 col-sm-push-4 clearfix text-center">
+                <ul class="list-inline links-holder">
+                    <li><a href="index.html">Back</a></li>
+                    <li>
+                        <a href="#" data-toggle="modal" data-target="#myModal">Share</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="pag pag-prev col-sm-4 col-xs-6 col-sm-pull-4">
+                <a href="single-project-assistant.html" class="link prev-post">
+                    <span class="arrow left-arrow"></span>Assistant
+                </a>
+            </div>
+            <div class="pag pag-next col-sm-4 col-xs-6 text-right">
+                <a href="single-project-field.html" class="link add next-post">Field Notes
+                    <span class="arrow right-arrow"></span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- ./Pagination & action buttons -->
+
+    <!-- Go top button/arrow -->
+    <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
+
+</main>
