0a6ade75db10e59b92e46558b12e7b2d126eb0d4
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 14:44:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,63 +4,6 @@
 <main id="main" class="container">
     <div class="row article-holder">
         <!-- Post -->
-
-        <?php
-
-        $paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
-
-        if (is_tag()) {
-            $args = array(
-                'tag_id' => get_queried_object_id(),
-                'posts_per_page' => 4,
-                'paged' => $paged
-            );
-
-        } else {
-            $args = array(
-                'cat' => get_queried_object_id(),
-                'posts_per_page' => 4,
-                'paged' => $paged
-            );
-
-        }
-
-        $i = 1;
-
-        $latest_blog_posts = new WP_Query($args);
-
-        if ($latest_blog_posts->have_posts()):
-            while ($latest_blog_posts->have_posts()):
-                $latest_blog_posts->the_post();
-
-
-
-                foreach ((get_the_category()) as $category) {
-                    $cat_name = $category->cat_name;
-                    $cat_link = get_category_link($category->term_id);
-                }
-
-                $src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), array(2600, 1000), false, '');
-
-                ?>
-
-
-                <?php $i++; endwhile; endif; ?>
-
-        <?php
-
-
-        $big = 999999999; // need an unlikely integer
-        
-        echo paginate_links(array(
-            'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-            'format' => '?paged=%#%',
-            'current' => max(1, get_query_var('paged')),
-            'total' => $latest_blog_posts->max_num_pages
-        ));
-        ?>
-
-
         <?php
         // if everything is in place and ready, let's start the loop
         if (have_posts()):
@@ -103,6 +46,9 @@
 
 
             <!-- fim post -->
+
+
+
             <!-- Pagination & action buttons -->
             <div class="container link-area">
                 <div class="row links-section">
@@ -113,28 +59,24 @@
                             </li>
                         </ul>
                     </div>
-
                     <div class="pag pag-prev col-sm-4 col-xs-6 col-sm-pull-4">
-                        <a href="<?php echo get_permalink($prevID); ?>" class="link prev-post">
-                            <span class="arrow left-arrow"></span>Older Posts
-                        </a>
+                        <span class="arrow left-arrow"></span><span
+                            class="link prev-post"><?php previous_posts_link('Older posts'); ?></span>
+
                     </div>
-
-
-
                     <div class="pag pag-next col-sm-4 col-xs-6 text-right">
-                        <a href="<?php echo get_permalink($nextID); ?>" class="link add next-post">Newer Posts
-                            <span class="arrow right-arrow"></span>
-                        </a>
+                        <span class="arrow right-arrow"></span><span
+                            class="link add next-post"><?php next_posts_link('Newer posts'); ?></span>
+
                     </div>
+                </div>
+            </div>
+            <!-- ./Pagination & action buttons -->
 
 
-
-                    <!-- ./Pagination & action buttons -->
-
-                <?php endif; ?>
-                <!-- Go top button/arrow -->
-                <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
+        <?php endif; ?>
+        <!-- Go top button/arrow -->
+        <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
 
 </main>
 <?php get_footer(); ?>
