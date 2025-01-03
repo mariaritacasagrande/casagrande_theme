03eac4743174ccdd9db567ac71b4e1ea92ff98d5
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 12:32:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -48,57 +48,7 @@
 
         <!-- fim post -->
 
-        <!-- Começa o Loop. -->
-        <?php if (have_posts()):
-            while (have_posts()):
-                the_post(); ?>
 
-                <!-- O código a seguir testa se o post atual pertence à categoria 3 -->
-                <!-- Se pertence, a classe css da DIV será definida como "post-cat-three". -->
-                <!-- Se não, a classe da DIV será definida como "post". -->
-                <?php if (in_category('3')) { ?>
-                    <div class="post-cat-three">
-                    <?php } else { ?>
-                        <div class="post">
-                        <?php } ?>
-
-                        <article class="post-article col-sm-4 col-xs-12">
-                            <div class="img-holder">
-                                <a href="<?php the_permalink() ?>" rel="bookmark"
-                                    title="Permanent Link to <?php the_title_attribute(); ?>">
-                                    <img src="images/blog-feat-img/img12.jpg" width="455" height="640" alt="image description"
-                                        class="img-responsive lazy">
-                                    <div class="caption text-center text-lowercase">
-                                        <span class="holder"><i class="arrow right-arrow"></i></span>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="text">
-                                <h2><a href="<?php the_permalink() ?>" rel="bookmark"
-                                        title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                                <p class="excerpt"><?php the_excerpt(); ?><span class="post-grad"></span></p>
-                                <div class="meta">
-                                    <em><a href="#">News</a> — <a href="#"><?php the_category(', '); ?></a></em>
-                                    <time><?php the_time('F jS, Y') ?></time>
-                                </div>
-                            </div>
-                        </article>
-                    </div> <!-- Fecha a primeira DIV -->
-
-                    <!-- Termina o Loop (mas repare no "else" - veja próxima linha) -->
-                <?php endwhile; else: ?>
-
-                <!-- O primeiro IF testou para ver se havia posts a serem mostrados -->
-                <!-- Este ELSE diz ao WordPress o que fazer se não houver nenhum -->
-                <p>Sorry, no posts matched your criteria.</p>
-
-                <!-- Término verdadeiro do Loop -->
-            <?php endif; ?>
-
-
-
-
-        </div>
 
         <!-- Pagination & action buttons -->
         <div class="container link-area">
