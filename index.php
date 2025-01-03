2f5b18fd7da75b0dfe4c8162f0f0434d63067397
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 12:24:12 -0300

    Update

--- index.php
+++ index.php
@@ -1,129 +1,135 @@
 <?php get_header(); ?>
 
 <!-- Main content -->
-<div class="main-container">
-    <main id="main" class="portfolio-page">
-        <!-- Page head/intro title & subtitle -->
-        <div class="container heading-section">
-            <div class="intro big-heading parallax col-sm-12">
-                <h1>The Craft</h1>
-                <p>Crafting ideas to generate branding experiences.<br class="nobr">
-                    Büro trascends the beauty to achieve the function.</p>
-            </div>
-        </div>
-        <!-- ./Page head/intro title & subtitle -->
+<main id="main" class="container">
+    <div class="row article-holder">
+        <!-- Post -->
+        <?php
+        // if everything is in place and ready, let's start the loop
+        if (have_posts()):
+            while (have_posts()):
+                the_post(); ?>
 
-        <span id="prtfl-anchor" class="container go"></span>
-        <div class="container portfolio-grid">
-            <div class="row">
-                <div class="col-sm-6">
-                    <a class="fltrs-trggr go" href="#prtfl-anchor">
-                        <div class="filters-icon">
-                            <div class="icn-wrapper">
-                                <i class="eyecon"></i>
-                                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 16">
-                                    <circle cx="8" cy="8" r="6.215" transform="rotate(90 8 8)"></circle>
-                                </svg>
+                <?php static $count = 0;
+                if ($count == "n") {
+                    break;
+                } else { ?>
+
+
+                    <article class="post-article col-sm-4 col-xs-12">
+                        <div class="img-holder">
+                            <a href="<?php the_permalink() ?>" rel="bookmark"
+                                title="Permanent Link to <?php the_title_attribute(); ?>">
+                                <img src="images/blog-feat-img/img12.jpg" width="455" height="640" alt="image description"
+                                    class="img-responsive lazy">
+                                <div class="caption text-center text-lowercase">
+                                    <span class="holder"><i class="arrow right-arrow"></i></span>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="text">
+                            <h2><a href="<?php the_permalink() ?>" rel="bookmark"
+                                    title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                            <p class="excerpt"><?php the_excerpt(); ?><span class="post-grad"></span></p>
+                            <div class="meta">
+                                <em><a href="#">News</a> — <a href="#"><?php the_category(', '); ?></a></em>
+                                <time><?php the_time('F jS, Y') ?></time>
                             </div>
                         </div>
+                    </article>
+
+                    // here, we continue with the limiting of the number of displayed posts
+                    // each iteration of the loop increases the value of '$count' by one
+                    // the final two lines complete the loop and close the if statement
+
+                    <?php $count++;
+                } ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+
+
+
+        <!-- fim post -->
+
+        <!-- Começa o Loop. -->
+        <?php if (have_posts()):
+            while (have_posts()):
+                the_post(); ?>
+
+                <!-- O código a seguir testa se o post atual pertence à categoria 3 -->
+                <!-- Se pertence, a classe css da DIV será definida como "post-cat-three". -->
+                <!-- Se não, a classe da DIV será definida como "post". -->
+                <?php if (in_category('3')) { ?>
+                    <div class="post-cat-three">
+                    <?php } else { ?>
+                        <div class="post">
+                        <?php } ?>
+
+                        <article class="post-article col-sm-4 col-xs-12">
+                            <div class="img-holder">
+                                <a href="<?php the_permalink() ?>" rel="bookmark"
+                                    title="Permanent Link to <?php the_title_attribute(); ?>">
+                                    <img src="images/blog-feat-img/img12.jpg" width="455" height="640" alt="image description"
+                                        class="img-responsive lazy">
+                                    <div class="caption text-center text-lowercase">
+                                        <span class="holder"><i class="arrow right-arrow"></i></span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="text">
+                                <h2><a href="<?php the_permalink() ?>" rel="bookmark"
+                                        title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                                <p class="excerpt"><?php the_excerpt(); ?><span class="post-grad"></span></p>
+                                <div class="meta">
+                                    <em><a href="#">News</a> — <a href="#"><?php the_category(', '); ?></a></em>
+                                    <time><?php the_time('F jS, Y') ?></time>
+                                </div>
+                            </div>
+                        </article>
+                    </div> <!-- Fecha a primeira DIV -->
+
+                    <!-- Termina o Loop (mas repare no "else" - veja próxima linha) -->
+                <?php endwhile; else: ?>
+
+                <!-- O primeiro IF testou para ver se havia posts a serem mostrados -->
+                <!-- Este ELSE diz ao WordPress o que fazer se não houver nenhum -->
+                <p>Sorry, no posts matched your criteria.</p>
+
+                <!-- Término verdadeiro do Loop -->
+            <?php endif; ?>
+
+
+
+
+        </div>
+
+        <!-- Pagination & action buttons -->
+        <div class="container link-area">
+            <div class="row links-section">
+                <div class="col-sm-4 col-xs-12 col-sm-push-4 clearfix text-center">
+                    <ul class="list-inline links-holder">
+                        <li>
+                            <a href="#" data-toggle="modal" data-target="#myModal">Share</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="pag pag-prev col-sm-4 col-xs-6 col-sm-pull-4">
+                    <a href="#" class="link prev-post">
+                        <span class="arrow left-arrow"></span>Older Posts
                     </a>
                 </div>
-                <ul class="filter-nav">
-                    <!-- you can setup your filter categories in a very easy way -->
-                    <!-- just by adding 'data-filter' and your category as value -->
-                    <!-- for example: -->
-                    <!-- data-filter="branding" -->
-                    <li class="active"><a href="#prtfl-anchor" data-filter="all">All</a></li>
-                    <li><a href="#prtfl-anchor" data-filter="branding">Branding</a></li>
-                    <li><a href="#prtfl-anchor" data-filter="illustration">Illustration</a></li>
-                    <li><a href="#prtfl-anchor" data-filter="motion">Motion</a></li>
-                    <li><a href="#prtfl-anchor" data-filter="photography">Photography</a></li>
-                    <li><a href="#" data-filter="web">Web</a></li>
-                </ul>
-            </div>
-            <!-- Set here the columns number: 'cols-2' is the default value -->
-            <div class="js-ajax-holder projects cols-2">
-                <div class="row portfolio-list js-ajax-container">
-                    <!-- 'data-filter' attributes allow you to join the item with the category -->
-                    <!-- this category must be the same that previously within 'filter-nav' -->
-                    <div class="img-holder prjct-item" data-filter="branding">
-                        <a href="single-project-assistant.html">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/grid-projects/project-01.jpg" alt="image description"
-                                class="img-responsive" width="672" height="672">
-                            <span class="caption">Assistant<span>— branding</span></span>
-                        </a>
-                    </div>
-                    <div class="img-holder prjct-item" data-filter="illustration">
-                        <a href="single-project.html">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/grid-projects/project-06.jpg" alt="image description"
-                                class="img-responsive" width="672" height="448">
-                            <span class="caption">People by Leif<span>— branding</span></span>
-                        </a>
-                    </div>
-                    <div class="img-holder prjct-item" data-filter="branding">
-                        <a href="single-project-titelles.html">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/grid-projects/project-04.jpg" alt="image description"
-                                class="img-responsive" width="672" height="672">
-                            <span class="caption">Titelles al Born<span>— branding</span></span>
-                        </a>
-                    </div>
-                    <div class="img-holder prjct-item" data-filter="photography">
-                        <a href="single-project-field.html">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/grid-projects/project-02.jpg" alt="image description"
-                                class="img-responsive" width="672" height="448">
-                            <span class="caption">Anthropologie<span>— photography</span></span>
-                        </a>
-                    </div>
-                    <div class="img-holder prjct-item" data-filter="branding">
-                        <a href="single-project-jack.html">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/grid-projects/project-03.jpg" alt="image description"
-                                class="img-responsive" width="672" height="448">
-                            <span class="caption">Jack Wills<span>— branding</span></span>
-                        </a>
-                    </div>
-                    <div class="img-holder prjct-item" data-filter="photography">
-                        <a href="single-project-lovely.html">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/grid-projects/project-05.jpg" alt="image description"
-                                class="img-responsive" width="672" height="448">
-                            <span class="caption">Lovely Bastard<span>— photography</span></span>
-                        </a>
-                    </div>
-                    <div class="img-holder prjct-item" data-filter="motion">
-                        <a href="single-project-gil.html">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/grid-projects/project-07.jpg" alt="image description"
-                                class="img-responsive" width="672" height="448">
-                            <span class="caption">Gill Vicente<span>— motion</span></span>
-                        </a>
-                    </div>
-                    <div class="img-holder prjct-item" data-filter="branding">
-                        <a href="single-project-urbana.html">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/grid-projects/project-08.jpg" alt="image description"
-                                class="img-responsive" width="672" height="672">
-                            <span class="caption">Urbana<span>— branding</span></span>
-                        </a>
-                    </div>
-                    <div class="img-holder prjct-item" data-filter="motion">
-                        <a href="single-project-mutualismi.html">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/grid-projects/project-09.jpg" alt="image description"
-                                class="img-responsive" width="672" height="448">
-                            <span class="caption">Mutualismi<span>— motion</span></span>
-                        </a>
-                    </div>
-                </div>
-                <div class="row btn-block">
-                    <div class="grid-pag clearfix">
-                        <div class="next-post btn">
-                            <a href="inc/2.html">2</a>
-                        </div>
-                        <div class="prev-post btn"><a href="inc/2.html">1</a></div>
-                    </div>
+                <div class="pag pag-next col-sm-4 col-xs-6 text-right">
+                    <a href="#" class="link add next-post">Newer Posts
+                        <span class="arrow right-arrow"></span>
+                    </a>
                 </div>
             </div>
         </div>
+        <!-- ./Pagination & action buttons -->
 
         <!-- Go top button/arrow -->
         <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
-    </main>
 
-    <?php get_sidebar(); ?>
-    <?php get_footer(); ?>
+</main>
+<?php get_footer(); ?>
