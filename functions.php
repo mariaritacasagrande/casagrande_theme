8d8ad703ed54fd1f9b3e1aec8b99ce5993bc9d76
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 13:09:07 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -96,42 +96,6 @@ add_theme_support('automatic-feed-links');
 
 //prev-next
 
-function get_next_posts_link($label = null, $max_page = 0)
-{
-    global $paged, $wp_query;
-
-    if (!$max_page) {
-        $max_page = $wp_query->max_num_pages;
-    }
-
-    if (!$paged) {
-        $paged = 1;
-    }
-
-    $next_page = (int) $paged + 1;
-
-    if (null === $label) {
-        $label = __('Next Page &raquo;');
-    }
-
-    if (!is_single() && ($next_page <= $max_page)) {
-        /**
-         * Filters the anchor tag attributes for the next posts page link.
-         *
-         * @since 2.7.0
-         *
-         * @param string $attributes Attributes for the anchor tag.
-         */
-        $attr = apply_filters('next_posts_link_attributes', '');
-
-        return sprintf(
-            '<a href="%1$s" "%2$s">%3$s</a>',
-            next_posts($max_page, false),
-            $attr,
-            preg_replace('/&([^#])(?![a-z]{1,8};)/i', '&#038;$1', $label)
-        );
-    }
-}
 
 
 ?>
