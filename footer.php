dfa8ee83906a3b4408a24fd521bb10a89fbec99b
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 18:29:20 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -67,7 +67,7 @@
 
 <!-- Main scripts -->
 <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+<script src="../js/main.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.js"></script>
 <script>
