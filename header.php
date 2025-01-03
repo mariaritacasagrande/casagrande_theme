e7a1672766b23e4a59685b2045bb8ce7e6f230ad
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 08:55:43 -0300

    Updates

--- header.php
+++ header.php
@@ -24,12 +24,12 @@
 
 	<!-- Favicon -->
 	<link rel="apple-touch-icon-precomposed" sizes="144x144"
-		href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-144x144.png" />
+		href="<?php echo get_template_directory_uri(); ?><?php echo get_template_directory_uri(); ?>/apple-touch-icon-144x144.png" />
 	<link rel="apple-touch-icon-precomposed" sizes="152x152"
-		href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-152x152.png" />
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>favicon-32x32.png"
+		href="<?php echo get_template_directory_uri(); ?><?php echo get_template_directory_uri(); ?>/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png"
 		sizes="32x32" />
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>favicon-16x16.png"
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png"
 		sizes="16x16" />
 
 	<!-- Google Fonts -->
