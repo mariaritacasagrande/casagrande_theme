caba1c14d8b9ced57fcb15c03a83c738dbc8b697
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-03 22:55:11 -0300

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -39,7 +39,7 @@ if (!is_active_sidebar('sidebar-1')) {
             foreach ($recent_posts as $post_item): ?>
                <li>
                   <a class="inner-link href=" <?php echo get_permalink($post_item['ID']) ?>">
-                     <?php the_title(); ?>
+                     <?php the_title($post_item['ID']); ?>
                   </a>
                </li>
             <?php endforeach; ?>
