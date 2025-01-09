a69bf1ecded5c91770e7fb67c7f0ba708226f8b7
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-09 15:13:17 -0300

    Update single-project.php

--- single-project.php
+++ single-project.php
@@ -62,9 +62,9 @@ get_header(); ?>
                     </div>
 
                     <div class="pag pag-prev col-md-4 col-xs-6 col-md-pull-4">
-                        <a href="<?php echo previous_post_link(); ?>">
-                            <span class="arrow left-arrow"></span>
-                        </a>
+
+                        <span class="arrow left-arrow"><?php previous_post_link(); ?></span>
+
                     </div>
                     <div class="pag pag-next col-md-4 col-xs-6 text-right">
                         <a href="<?php echo next_post_link(); ?>">
