2f307928dd6dfe39db75acc6db6b4485121c1e4f
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-09 13:43:33 -0300

    Update single-project.php

--- single-project.php
+++ single-project.php
@@ -32,54 +32,54 @@ get_header(); ?>
                                                 <span>in <?php the_category(', '); ?></span> — <?php the_tags(', '); ?>
                                             </em>
                                         </div>
-
-                                        <div class="post-body col-md-8 col-md-push-4">
-                                            <?php the_content(); ?>
-                                        </div>
+                                    </header>
                                 </div>
-                                <!-- Termina o Loop (mas repare no "else" - veja próxima linha) -->
-                            <?php endwhile; else: ?>
+                                <div class="post-body mar-btm-lg ">
+                                    <?php the_content(); ?>
+                                </div>
+                            </div>
+                            <!-- Termina o Loop (mas repare no "else" - veja próxima linha) -->
+                        <?php endwhile; else: ?>
 
-                            <!-- O primeiro IF testou para ver se havia posts a serem mostrados -->
-                            <!-- Este ELSE diz ao WordPress o que fazer se não houver nenhum -->
-                            <p>Sorry, no posts matched your criteria.</p>
+                        <!-- O primeiro IF testou para ver se havia posts a serem mostrados -->
+                        <!-- Este ELSE diz ao WordPress o que fazer se não houver nenhum -->
+                        <p>Sorry, no posts matched your criteria.</p>
 
-                            <!-- Término verdadeiro do Loop -->
-                        <?php endif; ?>
+                        <!-- Término verdadeiro do Loop -->
+                    <?php endif; ?>
                 </article>
-                </header>
-            </div>
-        </div>
-
-        <!-- Pagination & action buttons -->
-        <div class="container-fluid link-area">
-            <div class="container links-section">
-                <div class="col-md-4 col-xs-12 col-md-push-4 clearfix text-center">
-                    <ul class="list-inline links-holder">
-                        <li><a href="blog.html">Back</a></li>
-                        <li><a href="#" data-toggle="modal" data-target="#myModal">Share</a></li>
-                    </ul>
-                </div>
-
-                <div class="pag pag-prev col-md-4 col-xs-6 col-md-pull-4">
-                    <a href="#" class="link prev-post">
-                        <span class="arrow left-arrow"></span>Wellington bar
-                    </a>
-                </div>
-                <div class="pag pag-next col-md-4 col-xs-6 text-right">
-                    <a href="#" class="link add next-post">New project for Adidas
-                        <span class="arrow right-arrow"></span>
-                    </a>
-                </div>
 
             </div>
-        </div>
-        <!-- ./Pagination & action buttons -->
+
+            <!-- Pagination & action buttons -->
+            <div class="container-fluid link-area">
+                <div class="container links-section">
+                    <div class="col-md-4 col-xs-12 col-md-push-4 clearfix text-center">
+                        <ul class="list-inline links-holder">
+                            <li><a href="blog.html">Back</a></li>
+                            <li><a href="#" data-toggle="modal" data-target="#myModal">Share</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="pag pag-prev col-md-4 col-xs-6 col-md-pull-4">
+                        <a href="#" class="link prev-post">
+                            <span class="arrow left-arrow"></span>Wellington bar
+                        </a>
+                    </div>
+                    <div class="pag pag-next col-md-4 col-xs-6 text-right">
+                        <a href="#" class="link add next-post">New project for Adidas
+                            <span class="arrow right-arrow"></span>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+            <!-- ./Pagination & action buttons -->
 
 
 
-        <!-- Go top button/arrow -->
-        <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
+            <!-- Go top button/arrow -->
+            <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
 
 </main>
 
