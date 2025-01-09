967ca04c83053310238b4fab09ea18dc660906a9
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-09 14:52:40 -0300

    Update single-project.php

--- single-project.php
+++ single-project.php
@@ -63,13 +63,13 @@ get_header(); ?>
 
                     <div class="pag pag-prev col-md-4 col-xs-6 col-md-pull-4">
                         <a href="<?php echo previous_posts_link(''); ?>"
-                            class="link prev-post"><?php previous_posts_link(); ?>
+                            class="link prev-post"><?php previous_posts_link('Older Posts'); ?>
                             <span class="arrow left-arrow"></span>
                         </a>
                     </div>
                     <div class="pag pag-next col-md-4 col-xs-6 text-right">
                         <a href="<?php echo next_posts_link(''); ?>"
-                            class="link add next-post"><?php next_posts_link(); ?>
+                            class="link add next-post"><?php next_posts_link('Newer Posts'); ?>
                             <span class="arrow right-arrow"></span>
                         </a>
                     </div>
