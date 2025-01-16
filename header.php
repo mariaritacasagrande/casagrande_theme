8cebe587b07670423b8d78ad33939136b38f36ff
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-15 22:39:13 -0300

    Update header.php

--- header.php
+++ header.php
@@ -77,11 +77,10 @@
 					</div>
 					<!-- Main navigation -->
 					<div class="collapse navbar-collapse" id="navbar">
-
 						<?php
-						$menus = wp_nav_menu(array('theme_location' => 'header-menu'));
+						$menus = wp_get_nav_menus(array('theme_location' => 'header-menu'));
 						foreach ($menus as $menu /** @var WP_Term $menu */) {
-							$menu_items = wp_nav_menu_items($menu->term_id);
+							$menu_items = wp_get_nav_menu_items($menu->term_id);
 							if (!empty($menu_items)) {
 								echo '<ul class="nav navbar-nav">';
 								foreach ($menu_items as $menu_item) {
