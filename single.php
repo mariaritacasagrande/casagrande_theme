497043c33766c53cb3e43d2227d20a410af18809
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-17 20:17:57 -0300

    Update single.php

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
  */
 get_header(); ?>
 
-<div class="main-container"><!-- Abre .main-container -->
+<div class="main-container">
 
     <!-- Main content -->
     <main id="main">
@@ -13,27 +13,26 @@ get_header(); ?>
             while (have_posts()):
                 the_post(); ?>
 
-                <!-- Verifica a categoria 3 para adicionar classe diferente -->
+                <!-- O código a seguir testa se o post atual pertence à categoria 3 -->
+                <!-- Se pertence, a classe css da DIV será definida como "post-cat-three". -->
+                <!-- Se não, a classe da DIV será definida como "post". -->
                 <?php if (in_category('3')) { ?>
-                    <div class="post-cat-three"><!-- Abre .post-cat-three -->
+                    <div class="post-cat-three">
                     <?php } else { ?>
-                        <div class="post"><!-- Abre .post -->
+                        <div class="post">
                         <?php } ?>
-                        <div id="the-post" class="container"><!-- Abre #the-post .container -->
+                        <div id="the-post" class="container">
                             <article class="post-main">
                                 <div class="post-entry">
                                     <div class="heading-section">
                                         <header class="header big-heading parallax">
                                             <h1><?php the_title(); ?></h1>
                                             <div class="meta">
-                                                <?php the_time('F jS, Y'); ?>
+                                                <?php the_time('F jS, Y') ?>
                                                 <em>
-                                                    post written by
-                                                    <a href="#" class="author">
-                                                        <?php the_author_posts_link(); ?>
-                                                    </a>
-                                                    <span>in <?php the_category(', '); ?></span>
-                                                    — <?php the_tags('', ', '); ?>
+                                                    post written by <a href="#"
+                                                        class="author"><?php the_author_posts_link() ?></a>
+                                                    <span>in <?php the_category(', '); ?></span> — <?php the_tags(', '); ?>
                                                 </em>
                                             </div>
                                         </header>
@@ -42,141 +41,147 @@ get_header(); ?>
                                         <?php the_content(); ?>
                                     </div>
                                 </div>
-                            </article>
-                        </div><!-- Fecha #the-post .container -->
-                    </div><!-- Fecha .post-cat-three OU .post -->
+                                <!-- Termina o Loop (mas repare no "else" - veja próxima linha) -->
+                            <?php endwhile; else: ?>
 
-                <?php endwhile; else: ?>
-                <p>Sorry, no posts matched your criteria.</p>
-            <?php endif; ?>
+                            <!-- O primeiro IF testou para ver se havia posts a serem mostrados -->
+                            <!-- Este ELSE diz ao WordPress o que fazer se não houver nenhum -->
+                            <p>Sorry, no posts matched your criteria.</p>
 
-            <!-- Sidebar -->
-            <aside class="blog-sidebar col-md-3 col-md-pull-8">
-                <ul class="list-unstyled">
-                    <?php if (is_active_sidebar('blog-sidebar')): ?>
-                        <?php dynamic_sidebar('blog-sidebar'); ?>
-                    <?php else: ?>
-                        <li class="widget">
-                            <h3 class="widget_title"><?php _e('Example Widget', 'casagrande'); ?></h3>
-                            <p><?php _e('Add widgets to this area from the WordPress admin panel.', 'casagrande'); ?></p>
-                        </li>
-                    <?php endif; ?>
-                </ul>
-            </aside>
-            <!-- /.blog-sidebar -->
-
-            <!-- Pagination & action buttons -->
-            <div class="container-fluid link-area">
-                <div class="container links-section">
-                    <div class="col-md-4 col-xs-12 col-md-push-4 clearfix text-center">
-                        <ul class="list-inline links-holder">
-                            <li><a href="blog.html">Back</a></li>
-                            <li><a href="#" data-toggle="modal" data-target="#myModal">Share</a></li>
+                            <!-- Término verdadeiro do Loop -->
+                        <?php endif; ?>
+                    </article>
+                    <!-- sidebar -->
+                    <aside class="blog-sidebar col-md-3 col-md-pull-8">
+                        <ul class="list-unstyled">
+                            <?php if (is_active_sidebar('blog-sidebar')): ?>
+                                <?php dynamic_sidebar('blog-sidebar'); ?>
+                            <?php else: ?>
+                                <li class="widget">
+                                    <h3 class="widget_title"><?php _e('Example Widget', 'casagrande'); ?></h3>
+                                    <p><?php _e('Add widgets to this area from the WordPress admin panel.', 'casagrande'); ?>
+                                    </p>
+                                </li>
+                            <?php endif; ?>
                         </ul>
-                    </div>
+                    </aside>
 
-                    <div class="pag pag-prev col-md-4 col-xs-6 col-md-pull-4">
-                        <a href="#" class="link prev-post">
-                            <span class="arrow left-arrow"></span>Wellington bar
-                        </a>
-                    </div>
-                    <div class="pag pag-next col-md-4 col-xs-6 text-right">
-                        <a href="#" class="link add next-post">
-                            New project for Adidas <span class="arrow right-arrow"></span>
-                        </a>
+                    <!-- /.blog-sidebar -->
+                </div>
+
+                <!-- Pagination & action buttons -->
+                <div class="container-fluid link-area">
+                    <div class="container links-section">
+                        <div class="col-md-4 col-xs-12 col-md-push-4 clearfix text-center">
+                            <ul class="list-inline links-holder">
+                                <li><a href="blog.html">Back</a></li>
+                                <li><a href="#" data-toggle="modal" data-target="#myModal">Share</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="pag pag-prev col-md-4 col-xs-6 col-md-pull-4">
+                            <a href="#" class="link prev-post">
+                                <span class="arrow left-arrow"></span>Wellington bar
+                            </a>
+                        </div>
+                        <div class="pag pag-next col-md-4 col-xs-6 text-right">
+                            <a href="#" class="link add next-post">New project for Adidas
+                                <span class="arrow right-arrow"></span>
+                            </a>
+                        </div>
+
                     </div>
                 </div>
-            </div>
-            <!-- ./Pagination & action buttons -->
+                <!-- ./Pagination & action buttons -->
 
-            <!-- Comments -->
-            <div class="container comments-section">
-                <div class="col-md-5">
-                    <div class="comments-col">
-                        <div class="heading-block">
-                            <h4><?php _e('Let us know your thoughts about this topic', 'casagrande'); ?></h4>
-                            <span>
+                <!-- Comments -->
+                <div class="container comments-section">
+                    <div class="col-md-5">
+                        <div class="comments-col">
+                            <div class="heading-block">
+                                <h4><?php _e('Let us know your thoughts about this topic', 'casagrande'); ?></h4>
+                                <span>
+                                    <?php
+                                    printf(
+                                        _n('%s Comment', '%s Comments', get_comments_number(), 'casagrande'),
+                                        get_comments_number()
+                                    );
+                                    ?>
+                                </span>
+                            </div>
+                            <?php if (comments_open()): ?>
                                 <?php
-                                printf(
-                                    _n('%s Comment', '%s Comments', get_comments_number(), 'casagrande'),
-                                    get_comments_number()
-                                );
-                                ?>
-                            </span>
-                        </div>
-                        <?php if (comments_open()): ?>
-                            <?php
-                            $comment_form_args = array(
-                                'title_reply' => '',
-                                'label_submit' => __('Send', 'casagrande'),
-                                'comment_field' => '
+                                $comment_form_args = array(
+                                    'title_reply' => '',
+                                    'label_submit' => __('Send', 'casagrande'),
+                                    'comment_field' => '
                                 <div class="input-row">
                                     <label for="comment">' . __('Comment', 'casagrande') . '</label>
                                     <textarea id="comment" name="comment" cols="30" rows="10" placeholder="' . __('Join the conversation', 'casagrande') . '" class="form-control required-field"></textarea>
                                 </div>',
-                                'fields' => array(
-                                    'author' => '
+                                    'fields' => array(
+                                        'author' => '
                                     <div class="input-row">
                                         <label for="author">' . __('Name', 'casagrande') . '</label>
                                         <input id="author" name="author" type="text" placeholder="' . __('Your Name', 'casagrande') . '" class="form-control required-field" />
                                     </div>',
-                                    'email' => '
+                                        'email' => '
                                     <div class="input-row">
                                         <label for="email">' . __('Email', 'casagrande') . '</label>
                                         <input id="email" name="email" type="email" placeholder="' . __('Enter your e-mail', 'casagrande') . '" class="form-control required-email" />
                                     </div>',
-                                ),
-                                'submit_button' => '
+                                    ),
+                                    'submit_button' => '
                                 <div id="submit-btn">
                                     <input name="%1$s" type="submit" id="%2$s" value="%4$s" />
                                     <i class="arrow right-arrow"></i>
                                 </div>',
-                            );
-                            comment_form($comment_form_args);
-                            ?>
-                        <?php else: ?>
-                            <p><?php _e('Comments are closed for this post.', 'casagrande'); ?></p>
-                        <?php endif; ?>
-                    </div>
-                </div>
-
-                <!-- Listagem de comentários -->
-                <div class="col-md-7 col-md-8">
-                    <div class="commentlist">
-                        <?php if (have_comments()): ?>
-                            <h3>
-                                <?php
-                                printf(
-                                    _n('%1$s Comment', '%1$s Comments', get_comments_number(), 'casagrande'),
-                                    number_format_i18n(get_comments_number())
                                 );
+                                comment_form($comment_form_args);
                                 ?>
-                            </h3>
-                            <div class="comments-wrapper">
-                                <?php
-                                wp_list_comments(array(
-                                    'style' => 'div',
-                                    'short_ping' => true,
-                                    'avatar_size' => 50,
-                                    'callback' => 'custom_comments_callback', // Verifique se a função existe mesmo
-                                ));
-                                ?>
-                            </div>
-                        <?php else: ?>
-                            <p>
-                                <?php _e('No comments yet. Be the first to share your thoughts!', 'casagrande'); ?>
-                            </p>
-                        <?php endif; ?>
+                            <?php else: ?>
+                                <p><?php _e('Comments are closed for this post.', 'casagrande'); ?></p>
+                            <?php endif; ?>
+                        </div>
                     </div>
+
+                    <!-- Listagem de comentários -->
+                    <div class="col-md-7 col-md-8">
+                        <div class="commentlist">
+                            <?php if (have_comments()): ?>
+                                <h3>
+                                    <?php
+                                    printf(
+                                        _n('%1$s Comment', '%1$s Comments', get_comments_number(), 'casagrande'),
+                                        number_format_i18n(get_comments_number())
+                                    );
+                                    ?>
+                                </h3>
+                                <div class="comments-wrapper">
+                                    <?php
+                                    wp_list_comments(array(
+                                        'style' => 'div',
+                                        'short_ping' => true,
+                                        'avatar_size' => 50,
+                                        'callback' => 'custom_comments_callback', // Verifique se a função existe mesmo
+                                    ));
+                                    ?>
+                                </div>
+                            <?php else: ?>
+                                <p>
+                                    <?php _e('No comments yet. Be the first to share your thoughts!', 'casagrande'); ?>
+                                </p>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                    <!-- ./Listagem de comentários -->
                 </div>
-                <!-- ./Listagem de comentários -->
-            </div>
-            <!-- ./Comments -->
+                <!-- ./Comments -->
 
-            <!-- Go top button/arrow -->
-            <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
-    </main><!-- Fecha #main -->
 
-</div><!-- Fecha .main-container -->
+                <!-- Go top button/arrow -->
+                <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
 
-<?php get_footer(); ?>
+    </main>
+
+    <?php get_footer(); ?>
