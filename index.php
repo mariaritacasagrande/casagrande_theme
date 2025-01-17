f3749ade61fd5ec2796f4ed0bb763dd731edac77
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-17 16:34:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,71 +1,84 @@
 <?php get_header(); ?>
 
 <!-- Main content -->
-
 <main id="main" class="container">
     <div class="row article-holder">
+        <!-- Post -->
         <?php
-        // Loop para exibir posts do WordPress
-        $query = new WP_Query(['post_type' => 'post', 'posts_per_page' => 6]);
-        if ($query->have_posts()):
-            while ($query->have_posts()):
-                $query->the_post(); ?>
-                <!-- Post -->
-                <article class="post-article col-sm-4 col-xs-12">
-                    <div class="img-holder">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php if (has_post_thumbnail()) {
-                                the_post_thumbnail('medium', ['class' => 'img-responsive lazy']);
-                            } else { ?>
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder.jpg" alt="No Image"
+        // if everything is in place and ready, let's start the loop
+        if (have_posts()):
+            while (have_posts()):
+                the_post(); ?>
+
+                <?php static $count = 0;
+                if ($count == "n") {
+                    break;
+                } else { ?>
+
+
+                    <article class="post-article col-sm-4 col-xs-12">
+                        <div class="img-holder">
+                            <a href="<?php the_permalink() ?>" rel="bookmark"
+                                title="Permanent Link to <?php the_title_attribute(); ?>">
+                                <img src="<?php the_post_thumbnail(); ?>" width="455" height="640" alt="<?php the_title(); ?>"
                                     class="img-responsive lazy">
-                            <?php } ?>
-                            <div class="caption text-center text-lowercase">
-                                <span class="holder"><i class="arrow right-arrow"></i></span>
+                                <div class="caption text-center text-lowercase">
+                                    <span class="holder"><i class="arrow right-arrow"></i></span>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="text">
+                            <h2><a href="<?php the_permalink() ?>" rel="bookmark"
+                                    title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                            <p class="excerpt"><?php the_excerpt(); ?><span class="post-grad"></span></p>
+                            <div class="meta">
+                                <em>
+                                    <?php the_category('- '); ?>
+                                </em>
+                                <time><?php the_time('F jS, Y') ?></time>
                             </div>
-                        </a>
+                        </div>
+                    </article>
+
+                    <?php $count++;
+                } ?>
+
+            <?php endwhile; ?>
+
+
+
+            <!-- fim post e inicio de paginaçao-->
+
+
+
+            <!-- Pagination & action buttons -->
+            <div class="container link-area">
+                <div class="row links-section">
+                    <div class="col-sm-4 col-xs-12 col-sm-push-4 clearfix text-center">
+                        <ul class="list-inline links-holder">
+                            <li>
+                                <a href="#" data-toggle="modal" data-target="#myModal">Share</a>
+                            </li>
+                        </ul>
                     </div>
-                    <div class="text">
-                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                        <p class="excerpt"><?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?></p>
-                        <div class="meta">
-                            <em><?php the_category(' — '); ?></em>
-                            <time datetime="<?php the_time('c'); ?>"><?php the_time('jS M, Y'); ?></time>
+                    <div class="pag pag-prev col-sm-4 col-xs-6 col-sm-pull-4">
+                        <div class="link prev-post">
+                            <span class="arrow left-arrow"></span><?php previous_posts_link('Older Posts'); ?>
+                        </div>
+
+                    </div>
+                    <div class="pag pag-next col-sm-4 col-xs-6 text-right">
+                        <div class="link add next-post">
+                            <span class="arrow right-arrow"></span><?php next_posts_link('Newer Posts'); ?>
                         </div>
                     </div>
-                </article>
-            <?php endwhile;
-            wp_reset_postdata();
-        else: ?>
-            <p><?php _e('No posts found', 'textdomain'); ?></p>
+                </div>
+            </div>
+            <!-- ./Pagination & action buttons -->
+
         <?php endif; ?>
-    </div>
+        <!-- Go top button/arrow -->
+        <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
 
-    <!-- Pagination & action buttons -->
-    <div class="container link-area">
-        <div class="row links-section">
-            <div class="col-sm-4 col-xs-12 col-sm-push-4 clearfix text-center">
-                <ul class="list-inline links-holder">
-                    <li>
-                        <a href="#" data-toggle="modal" data-target="#myModal">Share</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="pag pag-prev col-sm-4 col-xs-6 col-sm-pull-4">
-                <?php previous_posts_link('<span class="arrow left-arrow"></span> Older Posts'); ?>
-            </div>
-            <div class="pag pag-next col-sm-4 col-xs-6 text-right">
-                <?php next_posts_link('Newer Posts <span class="arrow right-arrow"></span>'); ?>
-            </div>
-        </div>
-    </div>
-    <!-- ./Pagination & action buttons -->
-
-    <!-- Go top button/arrow -->
-    <a href="#wrapper" class="btn-top go js-link">
-        <i class="arrow right-arrow"></i>
-    </a>
 </main>
-</div>
-
 <?php get_footer(); ?>
