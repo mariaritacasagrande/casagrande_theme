15d59b14b091ee0731185722d8f800ee2b8a938d
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-17 16:38:25 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,15 +5,12 @@ function casagrande_enqueue_scripts()
 {
     // Estilos principais
     wp_enqueue_style('casagrande-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
-    wp_enqueue_style('casagrande-css-style', get_template_directory_uri() . '/css/style.css', array(), '1.0', 'all');
-    wp_enqueue_style('casagrande-bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '1.0', 'all');
+    wp_enqueue_style('casagrande-custom', get_template_directory_uri() . '/css/custom.css', array(), '1.0', 'all');
 
 
     // Scripts
     wp_enqueue_script('jquery');
     wp_enqueue_script('casagrande-main', get_template_directory_uri() . '/js/main.js', array('jquery'), '1.0', true);
-    wp_enqueue_script('casagrande-bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array('jquery'), '1.0', true);
-    wp_enqueue_script('casagrande-plugins', get_template_directory_uri() . '/js/plugins.js', array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'casagrande_enqueue_scripts');
 
