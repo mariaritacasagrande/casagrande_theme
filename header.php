3a600b2e11c86360fd81f728e4033349a0f69d98
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-17 15:33:39 -0300

    update

--- header.php
+++ header.php
@@ -1,23 +1,11 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html lang="en">
 
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
-	<title><?php wp_title('|', true, 'right'); ?></title>
-
-	<!-- Favicon -->
-	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon">
-
-	<!-- Google Fonts -->
-	<link href='https://fonts.googleapis.com/css?family=Lora:400,400italic,700%7CWork+Sans:200,400,300,500'
-		rel='stylesheet'>
-
-	<!-- Styles -->
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
-
+	<title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
 
@@ -27,30 +15,25 @@
 			<nav class="navbar">
 				<div class="navbar-header">
 					<button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse"
-						class="navbar-toggle collapsed" type="button"><span></span></button>
-					<!-- Logo -->
+						class="navbar-toggle collapsed" type="button">
+						<span></span>
+					</button>
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
-							<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" width="27"
-								height="45" alt="<?php bloginfo('name'); ?>">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png"
+								alt="<?php bloginfo('name'); ?>">
 						</a>
 					</div>
 				</div>
-				<!-- Main navigation -->
 				<div class="collapse navbar-collapse" id="navbar">
 					<?php
-					wp_nav_menu(array(
-						'theme_location' => 'primary', // Local do menu
+					wp_nav_menu([
+						'theme_location' => 'main-menu',
 						'container' => false,
 						'menu_class' => 'nav navbar-nav',
-						'fallback_cb' => false,
-						'walker' => new Custom_Nav_Walker(), // Adiciona o Walker personalizado
-					));
+					]);
 					?>
 				</div>
-
-
-				<!-- ./Main navigation -->
 			</nav>
 		</div>
 	</header>
