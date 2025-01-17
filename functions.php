08d36ffad292d88f8b5d55b44a274add0b8abd22
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-17 15:59:18 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -53,19 +53,6 @@ function casagrande_register_sidebar()
 }
 add_action('widgets_init', 'casagrande_register_sidebar');
 
-//scripts e estilos
-
-function casagrande_enqueue_scripts()
-{
-    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
-    wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array('jquery'), null, true);
-    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array('jquery'), null, true);
-}
-add_action('wp_enqueue_scripts', 'casagrande_enqueue_scripts');
-
-
 // habilitar Widgets
 function casagrande_widgets_init()
 {
