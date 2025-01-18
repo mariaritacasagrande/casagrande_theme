34966bc39e1ffd93970881fa0dd5b030878f0b58
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-18 14:16:58 -0300

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php
+// 404.php
+get_header(); // carrega o cabeçalho padrão do tema
+?>
+<!-- Main content -->
+<main id="main" class="container mar-btm-xlg">
+    <!-- Page head/intro title & subtitle -->
+    <div class="row heading-section">
+        <div class="intro">
+            <div class="col-sm-6 col-sm-offset-6">
+                <small>we are sorry</small>
+                <h1>404</h1>
+                <p>We can´t find the page you are looking for.<br>
+                    By clicking <a href="javascript:history.go(-1)">here</a> you can head back.</p>
+                <p class="mar-top-xs">Come back home, Jimmy.</p>
+            </div>
+        </div>
+    </div>
+    <!-- Go top button/arrow -->
+    <a href="#wrapper" class="btn-top go js-link"><i class="arrow right-arrow"></i></a>
+</main>
+<!-- #/main content area -->
+<?php
+get_footer(); // carrega o rodapé padrão do tema
