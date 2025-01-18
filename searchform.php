90cd52beda8a2c2f67093f1fb7c775a8e0e5c6ef
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-18 14:11:23 -0300

    Create searchform.php

---
+++ searchform.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * searchform.php
+ * Template para exibir o formulário de pesquisa
+ * Será utilizado em widgets ou get_search_form()
+ */
+?>
+
+<li id="search" class="widget widget_search">
+    <form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>">
+        <input type="search" placeholder="Search" value="" name="s" id="s" />
+        <button type="submit" form="searchform" name="submit"><i class="icon-zoom"></i></button>
+    </form>
+</li>
