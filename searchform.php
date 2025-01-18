d78867fbfc8260d6639758c93bafd40efa00c5a7
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-18 19:08:23 -0300

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -8,7 +8,7 @@
 
 <li id="search" class="widget widget_search">
     <form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>">
-        <input type="search" placeholder="Search" value="" name="s" id="s" />
+        <input type="search" placeholder="<?php _e('Search', 'casagrande'); ?> " value="" name="s" id="s" />
         <button type="submit" form="searchform" name="submit"><i class="icon-zoom"></i></button>
     </form>
 </li>
