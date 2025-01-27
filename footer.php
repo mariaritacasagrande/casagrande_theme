9d85c4af97b8fcb5043b54c7de894d6367b32b21
Maria Rita Casagrande <mariarita@riseup.net>
2025-01-27 11:25:38 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -85,19 +85,17 @@
 <script type="text/javascript"
     src="//maps.googleapis.com/maps/api/js?key=AIzaSyCoBKcqNEb7_YQRec1y98lkent_nA5vOtA&amp;"></script>
 
-<!-- Google Analytics -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8MGK5J65X7"></script>
 <script>
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date(); a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    ga('create', 'UA-XXXXXXX-X', 'auto');
-    ga('send', 'pageview');
+  gtag('config', 'G-8MGK5J65X7');
 </script>
 <?php wp_footer(); ?>
 </body>
 
 </html>
+
